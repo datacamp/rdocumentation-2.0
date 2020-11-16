ccf395e3078fa6505ec3955cbfe9b1af206589d6
Nick Carchedi <nick.carchedi@gmail.com>
2020-11-16 10:41:12 -0700

    tweak tailwind css setup

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Nav from '../components/nav';
-
-export default function IndexPage() {
-  return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
-      </div>
-    </div>
-  );
+export default function Home() {
+  return <div>Home</div>;
 }
