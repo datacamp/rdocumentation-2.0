baf2380c8c21a346ef4e24f59302925b7b899f76
Nick Carchedi <nick.carchedi@gmail.com>
2020-11-16 10:37:50 -0700

    initial setup

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const links = [
   { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
   { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
+];
 
 export default function Nav() {
   return (
@@ -25,5 +25,5 @@ export default function Nav() {
         </ul>
       </ul>
     </nav>
-  )
+  );
 }
