d6ec3b35a5372994261db00119117703d3509f26
Nick Carchedi <nick.carchedi@gmail.com>
2020-11-25 09:27:05 -0700

    upgrade to tailwind v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
