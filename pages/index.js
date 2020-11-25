8ab2c92e4cd2877cd31e526e62ad425633fda8aa
Nick Carchedi <nick.carchedi@gmail.com>
2020-11-25 09:21:37 -0700

    add more data sources to home

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,83 @@
-export default function Home({ downloadsLast30, trendingLast7 }) {
+export default function Home({
+  topDownloadsLast30,
+  topTrendingLast7,
+  allDownloadsLast30,
+  allRDownloadsLast30,
+  latestRVersion,
+}) {
+  console.log({
+    topDownloadsLast30,
+    topTrendingLast7,
+    allDownloadsLast30,
+    allRDownloadsLast30,
+    latestRVersion,
+  });
   return (
-    <div className="flex">
-      <div className="w-1/2">
-        <h2 className="text-xl font-bold">Most Downloaded (Last 30 Days)</h2>
-        <pre className="text-xs">
-          {JSON.stringify(downloadsLast30, null, 2)}
-        </pre>
+    <>
+      <div className="flex justify-around">
+        <div>
+          <h2 className="text-xl font-bold">Most Downloaded (Last 30 Days)</h2>
+          <pre className="text-xs">
+            {JSON.stringify(topDownloadsLast30, null, 2)}
+          </pre>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Trending (Last 7 Days)</h2>
+          <pre className="text-xs">
+            {JSON.stringify(topTrendingLast7, null, 2)}
+          </pre>
+        </div>
       </div>
-      <div className="w-1/2">
-        <h2 className="text-xl font-bold">Trending (Last 7 Days)</h2>
-        <pre className="text-xs">{JSON.stringify(trendingLast7, null, 2)}</pre>
+      <div className="flex justify-around mt-10">
+        <div>
+          <h2 className="text-xl font-bold">
+            Total Package Downloads (Last 30 Days)
+          </h2>
+          <pre className="text-xs">
+            {JSON.stringify(allDownloadsLast30, null, 2)}
+          </pre>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">
+            Total R Downloads (Last 30 Days)
+          </h2>
+          <pre className="text-xs">
+            {JSON.stringify(allRDownloadsLast30, null, 2)}
+          </pre>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Latest R Version</h2>
+          <pre className="text-xs">
+            {JSON.stringify(latestRVersion, null, 2)}
+          </pre>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export async function getStaticProps() {
-  const downloadsLast30 = await fetcher(
+  const topDownloadsLast30 = await fetcher(
     'https://cranlogs.r-pkg.org/top/last-month/10'
   );
-  const trendingLast7 = await fetcher('https://cranlogs.r-pkg.org/trending');
+  const topTrendingLast7 = await fetcher('https://cranlogs.r-pkg.org/trending');
+  const allDownloadsLast30 = await fetcher(
+    'https://cranlogs.r-pkg.org/downloads/total/last-month'
+  );
+  const allRDownloadsLast30 = await fetcher(
+    'https://cranlogs.r-pkg.org/downloads/total/last-month/R'
+  );
+  const allRVersions = await fetcher('https://rversions.r-pkg.org/r-versions');
+
   return {
     props: {
-      downloadsLast30: downloadsLast30.downloads,
-      trendingLast7: trendingLast7.slice(0, 10),
+      topDownloadsLast30: topDownloadsLast30.downloads,
+      topTrendingLast7: topTrendingLast7.slice(0, 10),
+      allDownloadsLast30: allDownloadsLast30[0].downloads,
+      allRDownloadsLast30: allRDownloadsLast30[0].downloads,
+      latestRVersion: allRVersions.slice(-1)[0],
     },
   };
 }
