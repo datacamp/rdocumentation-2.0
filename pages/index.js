ce3c67d042f4c1f81c71f73767d848cc8fc95309
Nick Carchedi <nick.carchedi@gmail.com>
2020-12-21 11:16:04 -0700

    updates from new computer

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import HomeSearchBar from '../components/HomeSearchBar';
 
 export default function HomePage() {
   return (
-    <div className="w-2/3 mt-56 mx-auto">
+    <div className="w-3/4 mx-auto mt-56">
       <h1 className="text-3xl">
         Search all 22,432 R packages on CRAN and Bioconductor
       </h1>
