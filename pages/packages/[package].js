3bb1c8339ac8da20f3a447daf316b6d1a81df659
Nick Carchedi <nick.carchedi@gmail.com>
2020-12-29 15:49:30 -0700

    return 404 when package not found and no version

--- pages/packages/[package].js
+++ pages/packages/[package].js
@@ -4,20 +4,27 @@ export default function PackagePage() {
 
 // if no version is specified, then just redirect to the latest version
 export async function getServerSideProps({ params: { package: packageName } }) {
-  const versions = await fetch(
+  const res = await fetch(
     `https://www.rdocumentation.org/api/packages/${packageName}`
-  )
-    .then((res) => res.json())
-    // sort in descending order by version ID
-    .then((data) => data.versions.sort((a, b) => b.id - a.id));
+  );
 
-  // grab the latest version
-  const latestVersion = versions.slice(0, 1)[0].version;
-
-  return {
-    redirect: {
-      destination: `${packageName}/versions/${latestVersion}`,
-      permanent: false,
-    },
-  };
+  try {
+    const data = await res.json();
+    // if package is found, get the latest version
+    const latestVersion = data.versions
+      .sort((a, b) => b.id - a.id)
+      .slice(0, 1)[0].version;
+    // return a redirect to the latest version
+    return {
+      redirect: {
+        destination: `${packageName}/versions/${latestVersion}`,
+        permanent: true,
+      },
+    };
+  } catch (error) {
+    // if package doesn't exist, return a 404
+    return {
+      notFound: true,
+    };
+  }
 }
