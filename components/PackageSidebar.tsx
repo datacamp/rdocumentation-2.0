a74d06d03697c078c5648a21a4e5f7c7c76eb242
Nick Carchedi <nick.carchedi@gmail.com>
2021-01-12 15:42:42 -0700

    show toast on copy to clipboard

--- components/PackageSidebar.tsx
+++ components/PackageSidebar.tsx
@@ -1,5 +1,6 @@
 import { Select, SelectOption } from '@datacamp/waffles-form-elements';
 import { CopyIcon } from '@datacamp/waffles-icons';
+import { toast, ToastContainer } from '@datacamp/waffles-toast';
 import { format } from 'date-fns';
 import { useRouter } from 'next/router';
 import { FaGithub, FaHome } from 'react-icons/fa';
@@ -81,6 +82,7 @@ export default function PackageSidebar({
 
   function handleCopyLink() {
     copyTextToClipboard(linkToCurrentVersion);
+    toast({ intent: 'success', title: 'Copied to clipboard!' });
   }
 
   function handleChangeVersion(selectedVersion) {
@@ -88,151 +90,154 @@ export default function PackageSidebar({
   }
 
   return (
-    <div className="space-y-6">
-      <div>
-        <SidebarHeader>Copy Link</SidebarHeader>
-        <div className="relative mt-4 dark:text-dc-navy">
-          <button
-            className="absolute inset-y-0 left-0 flex items-center justify-center w-10 pl-2"
-            onClick={handleCopyLink}
-            type="button"
-          >
-            <CopyIcon size={18} />
-          </button>
-          <input
-            className="block w-full pl-10 text-gray-500 border-2 rounded-md border-dc-grey300"
-            readOnly
-            type="text"
-            value={linkToCurrentVersion}
-          />
-        </div>
-      </div>
-      <div>
-        <SidebarHeader>Version</SidebarHeader>
-        <Select name="version" onChange={handleChangeVersion} value={version}>
-          {versionsArray.map((v) => (
-            <SelectOption key={v} value={v}>
-              {v}
-            </SelectOption>
-          ))}
-        </Select>
-      </div>
-      {/* show install code if it's a CRAN package */}
-      {showInstallCode && (
+    <>
+      <ToastContainer />
+      <div className="space-y-6">
         <div>
-          <SidebarHeader>Install</SidebarHeader>
-          <div className="prose-sm prose sm:prose">
-            <pre>
-              <code>{`install.packages('${packageName}')`}</code>
-            </pre>
+          <SidebarHeader>Copy Link</SidebarHeader>
+          <div className="relative mt-4 dark:text-dc-navy">
+            <button
+              className="absolute inset-y-0 left-0 flex items-center justify-center w-10 pl-2"
+              onClick={handleCopyLink}
+              type="button"
+            >
+              <CopyIcon size={18} />
+            </button>
+            <input
+              className="block w-full pl-10 text-gray-500 border-2 rounded-md border-dc-grey300"
+              readOnly
+              type="text"
+              value={linkToCurrentVersion}
+            />
           </div>
         </div>
-      )}
-      {monthlyDownloads && (
         <div>
-          <SidebarHeader>Monthly Downloads</SidebarHeader>
-          <div className="flex items-baseline justify-between">
-            <div className="text-3xl font-light">
-              {downloadsLastMonth.toLocaleString()}
-            </div>
-            <div className="w-3/5">
-              <MonthlyDownloadsChart
-                isDark={isDark}
-                monthlyDownloads={monthlyDownloads}
-              />
-            </div>
-          </div>
-        </div>
-      )}
-      <div className="flex">
-        <div className="w-1/2">
           <SidebarHeader>Version</SidebarHeader>
-          <SidebarValue>{version}</SidebarValue>
+          <Select name="version" onChange={handleChangeVersion} value={version}>
+            {versionsArray.map((v) => (
+              <SelectOption key={v} value={v}>
+                {v}
+              </SelectOption>
+            ))}
+          </Select>
         </div>
-        <div className="w-1/2">
-          <SidebarHeader>License</SidebarHeader>
-          {/* TODO: strip down the license text? */}
-          <SidebarValue>{license}</SidebarValue>
-        </div>
-      </div>
-      {repository && (
-        <>
-          <div className="flex">
-            <div className="w-1/2">
-              <SidebarHeader>Issues</SidebarHeader>
-              <SidebarValue>
-                <a
-                  href={`${githubUrl}/issues`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {repository.issues.toLocaleString()}
-                </a>
-              </SidebarValue>
-            </div>
-            <div className="w-1/2">
-              <SidebarHeader>Pull Requests</SidebarHeader>
-              <SidebarValue>
-                <a
-                  href={`${githubUrl}/pulls`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {repository.pullRequests.toLocaleString()}
-                </a>
-              </SidebarValue>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="w-1/2">
-              <SidebarHeader>Stars</SidebarHeader>
-              <SidebarValue>
-                <a href={githubUrl} rel="noopener noreferrer" target="_blank">
-                  {repository.stars.toLocaleString()}
-                </a>
-              </SidebarValue>
-            </div>
-            <div className="w-1/2">
-              <SidebarHeader>Forks</SidebarHeader>
-              <SidebarValue>
-                <a href={githubUrl} rel="noopener noreferrer" target="_blank">
-                  {repository.forks.toLocaleString()}
-                </a>
-              </SidebarValue>
-            </div>
-          </div>
+        {/* show install code if it's a CRAN package */}
+        {showInstallCode && (
           <div>
-            <SidebarHeader>Repository</SidebarHeader>
-            <SidebarValue Icon={FaGithub}>
-              <a href={githubUrl} rel="noopener noreferrer" target="_blank">
-                {githubUrl}
+            <SidebarHeader>Install</SidebarHeader>
+            <div className="prose-sm prose sm:prose">
+              <pre>
+                <code>{`install.packages('${packageName}')`}</code>
+              </pre>
+            </div>
+          </div>
+        )}
+        {monthlyDownloads && (
+          <div>
+            <SidebarHeader>Monthly Downloads</SidebarHeader>
+            <div className="flex items-baseline justify-between">
+              <div className="text-3xl font-light">
+                {downloadsLastMonth.toLocaleString()}
+              </div>
+              <div className="w-3/5">
+                <MonthlyDownloadsChart
+                  isDark={isDark}
+                  monthlyDownloads={monthlyDownloads}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="flex">
+          <div className="w-1/2">
+            <SidebarHeader>Version</SidebarHeader>
+            <SidebarValue>{version}</SidebarValue>
+          </div>
+          <div className="w-1/2">
+            <SidebarHeader>License</SidebarHeader>
+            {/* TODO: strip down the license text? */}
+            <SidebarValue>{license}</SidebarValue>
+          </div>
+        </div>
+        {repository && (
+          <>
+            <div className="flex">
+              <div className="w-1/2">
+                <SidebarHeader>Issues</SidebarHeader>
+                <SidebarValue>
+                  <a
+                    href={`${githubUrl}/issues`}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {repository.issues.toLocaleString()}
+                  </a>
+                </SidebarValue>
+              </div>
+              <div className="w-1/2">
+                <SidebarHeader>Pull Requests</SidebarHeader>
+                <SidebarValue>
+                  <a
+                    href={`${githubUrl}/pulls`}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {repository.pullRequests.toLocaleString()}
+                  </a>
+                </SidebarValue>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="w-1/2">
+                <SidebarHeader>Stars</SidebarHeader>
+                <SidebarValue>
+                  <a href={githubUrl} rel="noopener noreferrer" target="_blank">
+                    {repository.stars.toLocaleString()}
+                  </a>
+                </SidebarValue>
+              </div>
+              <div className="w-1/2">
+                <SidebarHeader>Forks</SidebarHeader>
+                <SidebarValue>
+                  <a href={githubUrl} rel="noopener noreferrer" target="_blank">
+                    {repository.forks.toLocaleString()}
+                  </a>
+                </SidebarValue>
+              </div>
+            </div>
+            <div>
+              <SidebarHeader>Repository</SidebarHeader>
+              <SidebarValue Icon={FaGithub}>
+                <a href={githubUrl} rel="noopener noreferrer" target="_blank">
+                  {githubUrl}
+                </a>
+              </SidebarValue>
+            </div>
+          </>
+        )}
+        {homeUrl && (
+          <div>
+            <SidebarHeader>Homepage</SidebarHeader>
+            <SidebarValue Icon={FaHome}>
+              <a href={homeUrl} rel="noopener noreferrer" target="_blank">
+                {homeUrl}
               </a>
             </SidebarValue>
           </div>
-        </>
-      )}
-      {homeUrl && (
-        <div>
-          <SidebarHeader>Homepage</SidebarHeader>
-          <SidebarValue Icon={FaHome}>
-            <a href={homeUrl} rel="noopener noreferrer" target="_blank">
-              {homeUrl}
-            </a>
-          </SidebarValue>
-        </div>
-      )}
-      <div className="flex">
-        <div className="w-1/2">
-          <SidebarHeader>Maintainer</SidebarHeader>
-          <SidebarValue>{maintainer.name}</SidebarValue>
-        </div>
-        {lastPublished && (
-          <div className="w-1/2">
-            <SidebarHeader>Last Published</SidebarHeader>
-            <SidebarValue>{format(lastPublished, 'PPP')}</SidebarValue>
-          </div>
         )}
+        <div className="flex">
+          <div className="w-1/2">
+            <SidebarHeader>Maintainer</SidebarHeader>
+            <SidebarValue>{maintainer.name}</SidebarValue>
+          </div>
+          {lastPublished && (
+            <div className="w-1/2">
+              <SidebarHeader>Last Published</SidebarHeader>
+              <SidebarValue>{format(lastPublished, 'PPP')}</SidebarValue>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
