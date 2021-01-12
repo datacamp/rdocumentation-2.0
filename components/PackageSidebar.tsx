86f43a57f0ffc0f5deac7ab7f910967980ded6f0
Nick Carchedi <nick.carchedi@gmail.com>
2021-01-12 10:15:22 -0700

    make function page responsive

--- components/PackageSidebar.tsx
+++ components/PackageSidebar.tsx
@@ -131,11 +131,10 @@ export default function PackageSidebar({
               {downloadsLastMonth.toLocaleString()}
             </div>
             <div className="w-3/5">
-              {/* TODO: put the chart back */}
-              {/* <MonthlyDownloadsChart
+              <MonthlyDownloadsChart
                 isDark={isDark}
                 monthlyDownloads={monthlyDownloads}
-              /> */}
+              />
             </div>
           </div>
         </div>
