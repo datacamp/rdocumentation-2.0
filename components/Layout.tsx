e4b8e6db93dca29fa76e4f71cd9ce113129cc1b0
Nick Carchedi <nick.carchedi@gmail.com>
2021-01-13 07:09:22 -0700

    change page title template

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children, description, title }: Props) {
   return (
     <>
       <Head>
-        <title>{title ? `${title} | RDocumentation` : 'RDocumentation'}</title>
+        <title>{title ? `${title} - RDocumentation` : 'RDocumentation'}</title>
         {description && <meta content={description} name="description" />}
       </Head>
       <div className="flex flex-col max-w-screen-xl min-h-screen px-5 mx-auto overflow-x-hidden md:px-10">
