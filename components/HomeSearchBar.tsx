4af477e849a56cc85dc77ac15e87e88d6d900cd4
Nick Carchedi <nick.carchedi@gmail.com>
2021-01-24 14:07:09 -0700

    add a random placeholder in search bar

--- components/HomeSearchBar.tsx
+++ components/HomeSearchBar.tsx
@@ -6,7 +6,17 @@ type Props = {
   value: string;
 };
 
+const PLACEHOLDERS = [
+  { function: 'group_by', package: 'dplyr' },
+  { function: 'fread', package: 'data.table' },
+  { function: 'summary.glm', package: 'stats' },
+  { function: 'geom_point', package: 'ggplot2' },
+];
+
 export default function HomeSearchBar({ onChange, value }: Props) {
+  // select a random placeholder
+  const p = PLACEHOLDERS[Math.floor(Math.random() * PLACEHOLDERS.length)];
+
   return (
     <div className="relative mt-4 dark:text-dc-navy">
       <div className="absolute inset-y-0 left-0 items-center hidden pl-6 pointer-events-none sm:flex">
@@ -15,7 +25,7 @@ export default function HomeSearchBar({ onChange, value }: Props) {
       <input
         className="block w-full py-2 text-lg border-2 rounded-md sm:pl-16 md:text-2xl md:py-4 placeholder-dc-grey400 border-dc-grey300 focus:border-dc-blue focus:ring-dc-blue dark:focus:border-dc-green dark:focus:ring-dc-green"
         onChange={onChange}
-        placeholder="For example, try 'dplyr' or 'group_by'"
+        placeholder={`For example, try '${p.package}' or '${p.function}'`}
         type="text"
         value={value}
       />
