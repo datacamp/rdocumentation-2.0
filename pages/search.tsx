3b4b9d11ffbdc162a1f2b8d6fdbe71ed0bdd9294
Nick Carchedi <nick.carchedi@gmail.com>
2021-02-09 14:07:29 -0700

    use query param to fix see more

--- pages/search.tsx
+++ pages/search.tsx
@@ -34,30 +34,25 @@ type FunctionResult = {
 
 export default function SearchResults() {
   const router = useRouter();
-  const { q: searchTerm } = router.query;
+  const { p: page, q: searchTerm } = router.query;
   const { theme } = useContext(ThemeContext);
 
   const [packageResults, setPackageResults] = useState<PackageResult[]>([]);
   const [functionResults, setFunctionResults] = useState<FunctionResult[]>([]);
-  const [pagesShown, setPagesShown] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
-  // reset search results and page count when search term changes
-  useEffect(() => {
-    setPagesShown(1);
-    setPackageResults([]);
-    setFunctionResults([]);
-  }, [searchTerm]);
+  // if no page in query params, then we're on page 1
+  const pageNumber = page ? Number(page) : 1;
 
   // fetch first page of results and add pages as requested
   useEffect(() => {
     async function fetchResults() {
       setIsLoading(true);
-      console.log(`Fetching page ${pagesShown} for ${searchTerm}...`);
+      console.log(`Fetching page ${pageNumber} for ${searchTerm}...`);
       try {
         // fetch the data
         const resPackages = await fetch(
-          `https://www.rdocumentation.org/search_packages?q=${searchTerm}&page=${pagesShown}&latest=1`,
+          `https://www.rdocumentation.org/search_packages?q=${searchTerm}&page=${pageNumber}&latest=1`,
           {
             headers: {
               Accept: 'application/json',
@@ -65,7 +60,7 @@ export default function SearchResults() {
           },
         );
         const resFunctions = await fetch(
-          `https://www.rdocumentation.org/search_functions?q=${searchTerm}&page=${pagesShown}&latest=1`,
+          `https://www.rdocumentation.org/search_functions?q=${searchTerm}&page=${pageNumber}&latest=1`,
           {
             headers: {
               Accept: 'application/json',
@@ -75,9 +70,15 @@ export default function SearchResults() {
         // extract the results
         const { packages } = await resPackages.json();
         const { functions } = await resFunctions.json();
-        // append to the existing results
-        setPackageResults((prevState) => [...prevState, ...packages]);
-        setFunctionResults((prevState) => [...prevState, ...functions]);
+        if (pageNumber === 1) {
+          // if on first page, overwrite the results
+          setPackageResults(packages);
+          setFunctionResults(functions);
+        } else {
+          // otherwise, append to previous results
+          setPackageResults((prevState) => [...prevState, ...packages]);
+          setFunctionResults((prevState) => [...prevState, ...functions]);
+        }
         // set loading state to false
         setIsLoading(false);
       } catch (error) {
@@ -86,7 +87,7 @@ export default function SearchResults() {
     }
     // get results once search term exists
     if (searchTerm) fetchResults();
-  }, [searchTerm, pagesShown]);
+  }, [searchTerm, pageNumber]);
 
   return (
     <Layout title={searchTerm ? `Results for '${searchTerm}'` : ''}>
@@ -95,29 +96,41 @@ export default function SearchResults() {
         <div className="grid grid-cols-1 mt-5 md:grid-cols-2">
           <div className="pb-5 space-y-4 md:border-r md:space-y-5 md:pr-10">
             <h2 className="text-2xl">Packages</h2>
-            {packageResults.map((p: PackageResult) => (
-              <ClickableCard
-                description={p.description}
-                extraInfo={`version ${p.fields.version}`}
-                href={`/packages/${p.fields.package_name}/versions/${p.fields.version}`}
-                id={`${p.fields.package_name}-${p.fields.version}`}
-                key={`${p.fields.package_name}-${p.fields.version}`}
-                name={p.fields.package_name}
-              />
-            ))}
+            {isLoading || packageResults.length > 0 ? (
+              <>
+                {packageResults.map((p: PackageResult) => (
+                  <ClickableCard
+                    description={p.description}
+                    extraInfo={`version ${p.fields.version}`}
+                    href={`/packages/${p.fields.package_name}/versions/${p.fields.version}`}
+                    id={`${p.fields.package_name}-${p.fields.version}`}
+                    key={`${p.fields.package_name}-${p.fields.version}`}
+                    name={p.fields.package_name}
+                  />
+                ))}
+              </>
+            ) : (
+              <p className="italic text-gray-600">No packages found</p>
+            )}
           </div>
           <div className="pb-5 mt-5 space-y-4 md:mt-0 md:space-y-5 md:pl-10">
             <h2 className="text-2xl">Functions</h2>
-            {functionResults.map((f: FunctionResult) => (
-              <ClickableCard
-                description={f.title}
-                extraInfo={f.fields.package_name}
-                href={`/packages/${f.fields.package_name}/versions/${f.fields.version}/topics/${f.fields.name}`}
-                id={`${f.fields.name}-${f.fields.version}`}
-                key={`${f.fields.name}-${f.fields.version}`}
-                name={f.fields.name}
-              />
-            ))}
+            {isLoading || functionResults.length > 0 ? (
+              <>
+                {functionResults.map((f: FunctionResult) => (
+                  <ClickableCard
+                    description={f.title}
+                    extraInfo={f.fields.package_name}
+                    href={`/packages/${f.fields.package_name}/versions/${f.fields.version}/topics/${f.fields.name}`}
+                    id={`${f.fields.package_name}-${f.fields.name}-${f.fields.version}`}
+                    key={`${f.fields.package_name}-${f.fields.name}-${f.fields.version}`}
+                    name={f.fields.name}
+                  />
+                ))}
+              </>
+            ) : (
+              <p className="italic text-gray-600">No functions found</p>
+            )}
           </div>
         </div>
         {!isLoading && (
@@ -125,7 +138,15 @@ export default function SearchResults() {
             <ButtonGroup>
               <Button
                 appearance={theme === 'light' ? 'default' : 'inverted'}
-                onClick={() => setPagesShown(pagesShown + 1)}
+                onClick={() =>
+                  router.push(
+                    `/search?q=${searchTerm}&p=${pageNumber + 1}`,
+                    undefined,
+                    {
+                      scroll: false,
+                    },
+                  )
+                }
               >
                 See More
               </Button>
