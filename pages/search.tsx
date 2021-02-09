ef564c8443f2179e215bb59695ca782cb1b14125
Nick Carchedi <nick.carchedi@gmail.com>
2021-02-09 14:35:35 -0700

    paginate search results

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import Button, { ButtonGroup } from '@datacamp/waffles-button';
-import { ArrowUpIcon } from '@datacamp/waffles-icons';
+import { ArrowLeftIcon, ArrowRightIcon } from '@datacamp/waffles-icons';
 import fetch from 'isomorphic-fetch';
 import { useRouter } from 'next/router';
 import { useContext, useEffect, useState } from 'react';
@@ -39,16 +39,14 @@ export default function SearchResults() {
 
   const [packageResults, setPackageResults] = useState<PackageResult[]>([]);
   const [functionResults, setFunctionResults] = useState<FunctionResult[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  // if no page in query params, then we're on page 1
   const pageNumber = page ? Number(page) : 1;
+  const onFirstPage = pageNumber === 1;
+  const onLastPage = packageResults.length < 15 && functionResults.length < 15;
 
   // fetch first page of results and add pages as requested
   useEffect(() => {
     async function fetchResults() {
-      setIsLoading(true);
-      console.log(`Fetching page ${pageNumber} for ${searchTerm}...`);
       try {
         // fetch the data
         const resPackages = await fetch(
@@ -67,20 +65,10 @@ export default function SearchResults() {
             },
           },
         );
-        // extract the results
         const { packages } = await resPackages.json();
         const { functions } = await resFunctions.json();
-        if (pageNumber === 1) {
-          // if on first page, overwrite the results
-          setPackageResults(packages);
-          setFunctionResults(functions);
-        } else {
-          // otherwise, append to previous results
-          setPackageResults((prevState) => [...prevState, ...packages]);
-          setFunctionResults((prevState) => [...prevState, ...functions]);
-        }
-        // set loading state to false
-        setIsLoading(false);
+        setPackageResults(packages);
+        setFunctionResults(functions);
       } catch (error) {
         console.log(error);
       }
@@ -89,19 +77,31 @@ export default function SearchResults() {
     if (searchTerm) fetchResults();
   }, [searchTerm, pageNumber]);
 
+  function handlePreviousPage() {
+    if (onFirstPage) return;
+    router.push(`/search?q=${searchTerm}&p=${pageNumber - 1}`);
+  }
+
+  function handleNextPage() {
+    if (onLastPage) return;
+    router.push(`/search?q=${searchTerm}&p=${pageNumber + 1}`);
+  }
+
   return (
     <Layout title={searchTerm ? `Results for '${searchTerm}'` : ''}>
       <div className="w-full max-w-screen-lg mx-auto mt-8 md:mt-12">
-        <h1 className="text-lg">Search results for '{searchTerm}':</h1>
+        <h1 className="text-lg">
+          Page {pageNumber} of results for '{searchTerm}':
+        </h1>
         <div className="grid grid-cols-1 mt-5 md:grid-cols-2">
           <div className="pb-5 space-y-4 md:border-r md:space-y-5 md:pr-10">
             <h2 className="text-2xl">Packages</h2>
-            {isLoading || packageResults.length > 0 ? (
+            {packageResults.length > 0 ? (
               <>
                 {packageResults.map((p: PackageResult) => (
                   <ClickableCard
                     description={p.description}
-                    extraInfo={`version ${p.fields.version}`}
+                    extraInfo={`version: ${p.fields.version}`}
                     href={`/packages/${p.fields.package_name}/versions/${p.fields.version}`}
                     id={`${p.fields.package_name}-${p.fields.version}`}
                     key={`${p.fields.package_name}-${p.fields.version}`}
@@ -115,12 +115,12 @@ export default function SearchResults() {
           </div>
           <div className="pb-5 mt-5 space-y-4 md:mt-0 md:space-y-5 md:pl-10">
             <h2 className="text-2xl">Functions</h2>
-            {isLoading || functionResults.length > 0 ? (
+            {functionResults.length > 0 ? (
               <>
                 {functionResults.map((f: FunctionResult) => (
                   <ClickableCard
-                    description={f.title}
-                    extraInfo={f.fields.package_name}
+                    description={f.description}
+                    extraInfo={`package: ${f.fields.package_name}`}
                     href={`/packages/${f.fields.package_name}/versions/${f.fields.version}/topics/${f.fields.name}`}
                     id={`${f.fields.package_name}-${f.fields.name}-${f.fields.version}`}
                     key={`${f.fields.package_name}-${f.fields.name}-${f.fields.version}`}
@@ -133,29 +133,24 @@ export default function SearchResults() {
             )}
           </div>
         </div>
-        {!isLoading && (
+        {(packageResults.length > 0 || functionResults.length > 0) && (
           <div className="flex justify-center mt-6">
             <ButtonGroup>
               <Button
                 appearance={theme === 'light' ? 'default' : 'inverted'}
-                onClick={() =>
-                  router.push(
-                    `/search?q=${searchTerm}&p=${pageNumber + 1}`,
-                    undefined,
-                    {
-                      scroll: false,
-                    },
-                  )
-                }
+                disabled={onFirstPage}
+                onClick={handlePreviousPage}
               >
-                See More
+                <ArrowLeftIcon />
+                Previous Page
               </Button>
               <Button
                 appearance={theme === 'light' ? 'default' : 'inverted'}
-                onClick={() => window.scrollTo({ behavior: 'smooth', top: 0 })}
+                disabled={onLastPage}
+                onClick={handleNextPage}
               >
-                <ArrowUpIcon />
-                Back to Top
+                Next Page
+                <ArrowRightIcon />
               </Button>
             </ButtonGroup>
           </div>
