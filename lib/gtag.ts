93d218ccb47e5ec2ae854abc105ea8a96222bb11
Dieter De Mesmaeker <dieter@datacamp.com>
2021-02-24 11:56:27 +0100

    feat(add google tag manager and google analytics)

---
+++ lib/gtag.ts
@@ -0,0 +1,8 @@
+export const GA_TRACKING_ID = 'UA-41577880-1';
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
