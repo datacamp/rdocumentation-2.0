87bfaf3c6e6aefa940a09e62401b6bb855598f6a
Zaurbek Tsorojev <tsorojev.zaurbek@gmail.com>
2022-03-01 10:22:18 +0100

    chore: update workspace link to use rdocs source

--- pages/packages/[package]/versions/[version]/topics/[topic].tsx
+++ pages/packages/[package]/versions/[version]/topics/[topic].tsx
@@ -141,7 +141,7 @@ export default function TopicPage({ topicData }: Props) {
                 <h2>Examples</h2>
                 <a
                   className="absolute p-2 text-sm rounded-md top-0	right-0 hover:bg-green-400 md:p-3 md:text-base md:top-16 md:right-2.5"
-                  href={`https://app.datacamp.com/workspace/new?_tag=template&templateKey=r-base-rdocs&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_workspace`}
+                  href={`https://app.datacamp.com/workspace/new?_tag=rdocs&rdocsPath=${rdocsPath}&title=${name}&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_workspace`}
                   style={{
                     background: 'rgba(3, 239, 98)',
                     color: '#1f2937',
@@ -156,7 +156,7 @@ export default function TopicPage({ topicData }: Props) {
                 <p>
                   Instantly run the code above in your browser using{' '}
                   <a
-                    href={`https://app.datacamp.com/workspace/new?_tag=template&templateKey=r-base-rdocs&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_workspace`}
+                    href={`https://app.datacamp.com/workspace/new?_tag=rdocs&rdocsPath=${rdocsPath}&title=${name}&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_workspace`}
                     target="_blank"
                   >
                     DataCamp Workspace
