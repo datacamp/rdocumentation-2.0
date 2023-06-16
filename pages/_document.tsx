679c950eb6f7c0fcec296bc34a4d0cd440480624
Aaron Bates <aaron@aaronbates.me>
2023-06-16 12:05:56 +0100

    fix: remove duped gtag inject

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,8 +7,6 @@ import Document, {
   NextScript,
 } from 'next/document';
 
-import { GA_TRACKING_ID } from '../lib/gtag';
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -25,18 +23,9 @@ class MyDocument extends Document {
             name="google-site-verification"
           />
           <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
             `,
             }}
           />
