96b352835f59e1d8a71d762afd0f9e493cf8be4c
Aaron Bates <aaron@aaronbates.me>
2023-06-16 11:39:49 +0100

    feat: migrate to ga4

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,8 +1,1 @@
-export const GA_TRACKING_ID = 'UA-41577880-1';
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
+export const GA4_TRACKING_ID = 'G-NL07LSD2L8';
