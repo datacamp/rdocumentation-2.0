7b7bb2f9c413d7e8eedef05029495a4cfaa86530
Hervé Eerebout <herve@datacamp.com>
2024-04-18 14:22:21 +0200

    feat: support DataLab

--- pages/packages/[package]/versions/[version]/topics/[topic].tsx
+++ pages/packages/[package]/versions/[version]/topics/[topic].tsx
@@ -59,7 +59,7 @@ export default function TopicPage({ topicData }: Props) {
   const registerClicks = (label) => {
     ReactGA4.event({
       action: 'Click',
-      category: 'Run in workspace',
+      category: 'Run in DataLab',
       label,
     });
   };
@@ -167,7 +167,7 @@ export default function TopicPage({ topicData }: Props) {
                 <h2>Examples</h2>
                 <a
                   className="absolute p-2 text-sm rounded-md top-0	right-0 hover:bg-green-400 md:p-3 md:text-base md:top-16 md:right-2.5"
-                  href={`https://app.datacamp.com/workspace/preview?_tag=rdocs&rdocsPath=${rdocsPath}&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_workspace`}
+                  href={`https://www.datacamp.com/datalab/preview?_tag=rdocs&rdocsPath=${rdocsPath}&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_datalab`}
                   onClick={() => registerClicks('Button')}
                   style={{
                     background: 'rgba(3, 239, 98)',
@@ -190,11 +190,11 @@ export default function TopicPage({ topicData }: Props) {
                 <p>
                   Run the code above in your browser using{' '}
                   <a
-                    href={`https://app.datacamp.com/workspace/preview?_tag=rdocs&rdocsPath=${rdocsPath}&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_workspace`}
+                    href={`https://www.datacamp.com/datalab/preview?_tag=rdocs&rdocsPath=${rdocsPath}&utm_source=r-docs&utm_medium=docs&utm_term=${topic}&utm_content=run_example_in_datalab`}
                     onClick={() => registerClicks('Link')}
                     target="_blank"
                   >
-                    DataCamp Workspace
+                    DataLab
                   </a>
                 </p>
               </div>
