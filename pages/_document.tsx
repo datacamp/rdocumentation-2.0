1a6ce3370cc885c1c1de6535adcce11d52978c11
joachimzeelmaekers <>
2024-09-11 11:43:03 +0200

    refactor: undo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable class-methods-use-this */
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
-class MyDocument extends NextDocument {
-  // static async getInitialProps(ctx: DocumentContext) {
-  //   const initialProps = await Document.getInitialProps(ctx);
-  //   return { ...initialProps };
-  // }
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="en">
